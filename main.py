34873486d6664de288ab20afec26704e7bc78ce1
wind <jamwujustyle@gmail.com>
2024-10-27 17:02:06 +0500

    rm redundant files

--- main.py
+++
@@ -1,8 +0,0 @@
-from functionCalculator import calculator
-
-
-numOne = float(input("enter number 1: "))
-operand = input("enter operand: ")
-numTwo = float(input("enter number 2: "))
-
-print(calculator(numOne, operand, numTwo))
